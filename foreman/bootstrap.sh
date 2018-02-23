959e46b11f296ee2d790f756bcdc8c0076738d7e
Radim Janča <janca@ics.muni.cz>
2018-02-23 13:14:11 +0100

    workicng install of pm, fm todo

--- foreman/bootstrap.sh
+++ foreman/bootstrap.sh
@@ -3,5 +3,3 @@
 yum -y install https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm epel-release
 yum update -y
 yum install -y puppet-agent
-
-/opt/puppetlabs/puppet/bin/puppet module install puppetlabs-inifile
